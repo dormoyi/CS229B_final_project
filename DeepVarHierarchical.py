c329cc5df2c3aacf7ae3893964d15eeefeaa3d7a
nyss <ines.dormoy@student-cs.fr>
2023-11-30 17:59:03 -0800

    before addind dynnamic gratures

--- DeepVarHierarchical.py
+++ DeepVarHierarchical.py
@@ -6,15 +6,21 @@ from datetime import datetime
 import sys
 import os
 
-local_gluonts_path = "/home/nyss/disque_tera/1_sf/2 cs299b/CS229B_final_project/gluonts"
+# local_gluonts_path = "/home/nyss/disque_tera/1_sf/2 cs299b/CS229B_final_project/gluonts"
 
-# Add the local path to the Python path
-sys.path.insert(0, os.path.abspath(local_gluonts_path))
+# # Add the local path to the Python path
+# sys.path.insert(0, os.path.abspath(local_gluonts_path))
 
-# import my_gluonts.mx.model.deepvar_hierarchical
+USE_INT64_TENSOR_SIZE=1
+
 from gluonts.mx.model.deepvar_hierarchical import DeepVARHierarchicalEstimator
 from gluonts.mx.trainer import Trainer
 from gluonts.dataset.hierarchical import HierarchicalTimeSeries
+from gluonts.model.predictor import Predictor
+from pathlib import Path
+
+import gc
+gc.collect()
 
 
 sns.set_theme(style="darkgrid")
@@ -116,7 +122,20 @@ df_pivoted2 = df_pivoted2.fillna(method='backfill')
 assert S.shape[0] == len(ts_mapping)
 assert S.shape[1] == len(df_pivoted2.columns)
 
-df_pivoted2_trunc = df_pivoted2.iloc[-10000:]
+
+humidity_df = pd.read_csv("data/relative humidity.csv")
+temperature_df = pd.read_csv("data/temperature.csv")
+humidity_df["timestamp"] = pd.to_datetime(humidity_df["date"] + " " + (humidity_df["hr"] - 1).astype(str) + ":00:00", format="%d%b%Y %H:%M:%S")
+temperature_df["timestamp"] = pd.to_datetime(temperature_df["date"] + " " + (temperature_df["hr"] - 1).astype(str) + ":00:00", format="%d%b%Y %H:%M:%S")
+humidity_df = humidity_df.drop(columns=["date", "hr"])
+temperature_df = temperature_df.drop(columns=["date", "hr"])
+features_df = temperature_df.merge(humidity_df, on="timestamp", how="outer")
+print(len(features_df))
+# set timestamp as index
+features_df = features_df.set_index("timestamp")
+
+df_pivoted2_trunc = df_pivoted2.iloc[-2000:]
+features_df_trunc = features_df.iloc[-2000:]
 
 hts = HierarchicalTimeSeries(
     ts_at_bottom_level=df_pivoted2_trunc,
@@ -124,23 +143,131 @@ hts = HierarchicalTimeSeries(
 )
 
 dataset = hts.to_dataset()
-
-print("training")
-
+# 4096000000 vs. 2147483647 - 25000, 2000
+# 4096000000 vs. 2147483647 - 50000, 2000
+# 4,096,000,000 vs. 2147483647 - 10000, 2000
+# 4,096,000,000 vs. 2147483647 - 15999, 2000
+# 8192000000 vs. 2,147,483,647 - 25000, 4000
 prediction_length = 24
-estimator = DeepVARHierarchicalEstimator(
-    freq=hts.freq,
-    prediction_length=prediction_length,
-    trainer=Trainer(epochs=2),
+hts_train = HierarchicalTimeSeries(
+    ts_at_bottom_level=df_pivoted2_trunc.iloc[:-prediction_length, :],
+    S=S,
+)
+hts_test_label = HierarchicalTimeSeries(
+    ts_at_bottom_level=df_pivoted2_trunc.iloc[-prediction_length:, :],
     S=S,
 )
 
-# save the model and predict, add variables when training
+dataset_train = hts_train.to_dataset()
+# dataset_test_label = hts_test_label.to_dataset()
+
+predictor_input = hts_train.to_dataset()
+# predictor_input = hts_test_label.to_dataset()
 
 
-predictor = estimator.train(dataset)
+estimator = DeepVARHierarchicalEstimator(
+    freq=hts_train.freq,
+    prediction_length=prediction_length,
+    trainer=Trainer(epochs=1),
+    S=S,
+)
 
-forecast_it = predictor.predict(dataset)
+experiment = "test8760"
+
+def train(dataset):
+    print("Training")
+
+    
+    directory_path = Path(experiment)
+    directory_path.mkdir(parents=True, exist_ok=True)
+
+
+    predictor = estimator.train(dataset)
+
+    print('Saving the model')
+    predictor.serialize(Path(experiment))
+
+train(dataset_train)
+
+print('Loading the model: ', experiment)
+predictor = Predictor.deserialize(Path(experiment))
+
+print('Predicting')
+# forecast_it = predictor.predict(dataset)
 
 # There is only one element in `forecast_it` containing forecasts for all the time series in the hierarchy.
+# forecasts = next(forecast_it)
+
+
+forecast_it = predictor.predict(predictor_input)
+
+from gluonts.evaluation import MultivariateEvaluator
+
+evaluator = MultivariateEvaluator()
+agg_metrics, item_metrics = evaluator(
+    ts_iterator=[hts_test_label.ts_at_all_levels],
+    fcst_iterator=forecast_it,
+)
+
+print(
+    f"Mean (weighted) quantile loss over all time series: "
+    f"{agg_metrics['mean_wQuantileLoss']}"
+)
+
+print(
+    f"Mean (weighted) absolute error over the top time series: "
+    f"{agg_metrics['0_abs_error']/prediction_length}"
+)   
+
+# check MAE with missing values
+
+
+print(
+    f"Mean (weighted) squared error over the top time series: "
+    f"{agg_metrics['0_RMSE']}"
+)
+
+# then look at table to determine each one of the metrics 
+# print(agg_metrics.keys())
+
+# Plot the forecasts for the top time series in the hierarchy
+
+forecast_it = predictor.predict(predictor_input)
+
 forecasts = next(forecast_it)
+
+
+point_estimations = np.mean(forecasts.samples, axis=0)
+assert point_estimations.shape == (prediction_length, 160)
+top_series = point_estimations[ :, 0]
+assert top_series.shape == (prediction_length,)
+
+top_series_label = np.array(hts_test_label.ts_at_all_levels[0][-prediction_length:])
+
+# plt.figure(figsize=(12, 5))
+# plt.plot(top_series_label, label="target")
+# plt.plot(top_series, label="forecast")
+# plt.legend()
+# plt.grid(which="both")
+# plt.show()
+
+
+# use cs224n environment
+
+# add additional variables
+
+# get colab pro and run the whole model on it
+
+# check hyperparameters (and do some grid search?)
+# get loss and training curves 
+
+# get plots and add them to the report
+
+# evaluate reconciliation level
+
+# check what time-series to take out for benchmarking
+# add inference time in benchmarking (and memory usage)
+# QUESTIONS how is train test valid made especially inside the training (when is loss computed?)
+
+# get number of parameters in the model?
+
---
+++ myDeepVARHierarchicalEstimator.py
@@ -0,0 +1,11 @@
+
+"""
+override gluonts classes to
+- save the model
+- get training curves (loss/metrics)
+- 
+"""
+
+from gluonts.mx.model.deepvar_hierarchical import DeepVARHierarchicalEstimator
+
+
